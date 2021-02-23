db4e7cacbfe484d90c63308d9ecdee995fc9f51c
ParampreetP <parmar.parampreet@gmail.com>
2021-02-22 23:54:42 -0500

    Worked On more

--- MultipleScreens/app/src/main/java/edu/binghamton/multiplescreens/MainActivity.java
+++ MultipleScreens/app/src/main/java/edu/binghamton/multiplescreens/MainActivity.java
@@ -3,14 +3,20 @@ package edu.binghamton.multiplescreens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edittext;
+     EditText userName;
+     Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = (EditText) findViewById(R.id.editText);
+
     }
 }
