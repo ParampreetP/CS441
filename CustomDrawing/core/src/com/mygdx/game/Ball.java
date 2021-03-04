3dc7badb4d1c4f7590362100e8dbd48b5737a6b6
ParampreetP <parmar.parampreet@gmail.com>
2021-03-03 22:46:58 -0500

    CustomDrawing App done

--- CustomDrawing/core/src/com/mygdx/game/Ball.java
+++ CustomDrawing/core/src/com/mygdx/game/Ball.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 //https://stackoverflow.com/questions/42071541/how-to-make-a-texture-jump-up-and-return-to-starting-position-android-java-lib
 public class Ball {
     public static final float GRAVITY = -50;
-    public static final float BOUNCE_DAMPENING = 0.5f;
+    public static final float BOUNCE_DAMPENING = 0.4f;
 
     public final Vector2 position = new Vector2();
     public final Vector2 velocity = new Vector2();
@@ -19,6 +19,11 @@ public class Ball {
         if (position.y <= 0){ // hit ground, so bounce
             position.y = -position.y * BOUNCE_DAMPENING;
             velocity.y = -velocity.y * BOUNCE_DAMPENING;
+        }
+
+        if (position.y >= 900){//mario will not be able to fly to high out of screen
+            position.y = -position.y;
+
         }
     }
 }
