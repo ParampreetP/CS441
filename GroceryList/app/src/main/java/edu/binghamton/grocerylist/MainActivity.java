60c3fa6147813f18f253d5aa1883b5bd66541342
ParampreetP <parmar.parampreet@gmail.com>
2021-03-09 13:33:20 -0500

    Worked on grocerylist app

--- GroceryList/app/src/main/java/edu/binghamton/grocerylist/MainActivity.java
+++ GroceryList/app/src/main/java/edu/binghamton/grocerylist/MainActivity.java
@@ -1,14 +1,56 @@
 package edu.binghamton.grocerylist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> list = new ArrayList<>();
+    ListView list_view;
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list_view = findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list );
+        list_view.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.add_item:
+                //todo
+                additem();
+                break;
+        }
+
+        return true;
+    }
+
+    private void additem() {
+
     }
 }
