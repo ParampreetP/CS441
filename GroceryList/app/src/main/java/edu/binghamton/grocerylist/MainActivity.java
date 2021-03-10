877d00c348f5e19811892c34830834c855a47bcc
ParampreetP <parmar.parampreet@gmail.com>
2021-03-10 00:51:25 -0500

    Got add grocerylist button working

--- GroceryList/app/src/main/java/edu/binghamton/grocerylist/MainActivity.java
+++ GroceryList/app/src/main/java/edu/binghamton/grocerylist/MainActivity.java
@@ -1,13 +1,18 @@
 package edu.binghamton.grocerylist;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -51,6 +56,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void additem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add New Item");
 
+        View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_dialog, null, false);
+
+        builder.setView(v);
+        final EditText etItem = v.findViewById(R.id.etItem);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (!etItem.getText().toString().isEmpty()){
+                    list.add(etItem.getText().toString().trim());
+                    arrayAdapter.notifyDataSetChanged();
+                } else {
+                    etItem.setError("Add item");
+                }
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
