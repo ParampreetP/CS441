8ab249ac8363a67e2859ee65360cb901aafbf68b
ParampreetP <parmar.parampreet@gmail.com>
2021-03-18 22:11:43 -0400

    Worked on text to speech

--- SpeechToText/app/src/main/java/edu/binghamton/speechtotext/MainActivity.java
+++ SpeechToText/app/src/main/java/edu/binghamton/speechtotext/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private final int REQ_CODE = 100;
     TextView textView;
     ImageView speak;
+    EditText editText2;
+    Button button;
+    TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
         speak = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        editText2 = findViewById(R.id.edittext);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR){
+                        textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tospeak = editText2.getText().toString();
+                Toast.makeText(getApplicationContext(), tospeak, Toast.LENGTH_SHORT).show();
+                textToSpeech.speak(tospeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
 
         speak.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +66,19 @@ public class MainActivity extends AppCompatActivity {
                     startActivityForResult(intent, REQ_CODE);
                 } catch (ActivityNotFoundException a) {
                     Toast.makeText(getApplicationContext(),
-                            "Sorry your device not supported",
+                            "Sorry your device is not supported",
                             Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+    }
+    public void onPause(){
+        if(textToSpeech !=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
