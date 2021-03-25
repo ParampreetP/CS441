06b9c073df31c711ccc55ff694daab40f2598dda
ParampreetP <parmar.parampreet@gmail.com>
2021-03-25 01:40:34 -0400

    Worked on AD

--- AsteroidDestroyer/core/src/com/mygdx/game/Asteroid_Destroyer.java
+++ AsteroidDestroyer/core/src/com/mygdx/game/Asteroid_Destroyer.java
@@ -8,18 +8,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Asteroid_Destroyer extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture ship;
+	Texture ship, stars;
+	float shipH, shipW, shipX, shipY;
+	float velocity = 0.0f;
+	float gravity = 0.6f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		ship = new Texture("badlogic.jpg");
+		ship = new Texture("ship.png");
+		stars = new Texture("star.png");
+		shipW = Gdx.graphics.getWidth()/6;
+		shipH = Gdx.graphics.getHeight()/10;
+		shipX = Gdx.graphics.getWidth()/2;
+		shipY = Gdx.graphics.getWidth()/10;
+
+
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
-
+		batch.draw(stars, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(ship, shipX, shipY, shipW, shipH);
 
 
 		batch.end();
@@ -28,6 +39,7 @@ public class Asteroid_Destroyer extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
+		stars.dispose();
 		ship.dispose();
 	}
 }
